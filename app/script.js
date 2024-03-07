8729a1819aec28b677c0f2381c165846c5f7a8cf
joaolucasmoreira <jl7084139@gmail.com>
2024-03-06 22:10:21 -0300

    alteração front end

--- app/script.js
+++ app/script.js
@@ -2,38 +2,46 @@ let data = []
 
 const submitComment = (event) => {
     event.preventDefault();
-    
-    const author = inputAuthor.value
-    const comment = inputComment.value
 
-    data.push({ author:author, comment: comment})
+    const author = inputAuthor.value;
+    const comment = inputComment.value;
+
+    data.push({ author: author, comment: comment })
     console.log(data)
 
     loadComment()
 }
 
-const formComentarios = document.getElementById('formComment')
-formComentarios.addEventListener("submit", submitComment)
+const formComentario = document.getElementById('formComment')
+formComentario.addEventListener("submit", submitComment)
 
 const loadComment = () => {
     if (data) {
-        divDisplayComment()
+        displayComment();
     }
 }
 
-const divDisplayComment = () => {
-    const body = document.getElementsByTagName('body')[0]
-    data.forEach(elemento => {
-    const divDisplay = document.createElement('div')
-    divDisplay.className = "commentarios"
-    divDisplay.innerHTML = `
-    <div class="comentarios">
-    <p>Author:${elemento.author}</p>
-    <p>Comentario: ${elemento.comment}</p>
-    </div>
-    `
-    body.appendChild(divDisplay)
-})
+const displayComment = () => {
+    const divFeed = document.getElementById('comment-feed'); 
+    divFeed.innerHTML = ``
+    data.forEach(item => {
+        const divDisplay = document.createElement('div');
+        divDisplay.className = 'comentarios'
+        divDisplay.innerHTML = `
+        <div class="d-flex text-body-secondary pt-3">
+            <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                preserveAspectRatio="xMidYMid slice" focusable="false">
+                <title>Placeholder</title>
+                <rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c"
+                    dy=".3em">32x32</text>
+            </svg>
+            <p class="pb-3 mb-0 small lh-sm border-bottom">
+                <strong class="d-block text-gray-dark">${item.author}</strong>
+                ${item.comment}
+            </p>
+        </div>
+        `
+        divFeed.appendChild(divDisplay);
+    })
 }
-
-loadComment();
